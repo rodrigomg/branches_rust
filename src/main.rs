335acee05b6755cb2585981054ff3857ebd65d34
rmgarcia <rockdrigo.mtz@gmail.com>
2018-11-13 12:36:09 -0600

    adding mulple if example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     sample_if();
+    multiple_if();
 }
 
 fn sample_if() {
@@ -9,5 +10,19 @@ fn sample_if() {
         println!("Condition was true");
     } else {
         println!("Condition was false");
+    }
+}
+
+fn multiple_if() {
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3 or 2");
     }
 }
